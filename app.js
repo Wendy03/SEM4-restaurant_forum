b00773e29d78d7bbccbd8343f29c163b465e2a98
wendy03 <wendysu03@gmail.com>
2020-02-13 19:48:07 +0800

    Install pg and update production config

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const db = require('./models')
+const passport = require('./config/passport')
+const methodOverride = require('method-override')
 const app = express()
 
 const port = 3000
@@ -8,7 +14,26 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(__dirname + '/upload'))
+
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`Express is listening on http://localhost:${port}`)
 })
+
+require('./routes')(app, passport)
