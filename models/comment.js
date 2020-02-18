9ce3080c2d72f1cf1bea0b29783d0204a576fd54
wendy03 <wendysu03@gmail.com>
2020-02-18 20:25:48 +0800

    Add association among users, comment, restaurants

--- models/comment.js
+++ models/comment.js
@@ -1,12 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define('Comment', {
-    text: DataTypes.STRING,
-    UserId: DataTypes.INTEGER,
-    RestaurantId: DataTypes.INTEGER
-  }, {});
-  Comment.associate = function(models) {
-    // associations can be defined here
-  };
-  return Comment;
+    const Comment = sequelize.define('Comment', {
+        text: DataTypes.STRING,
+        UserId: DataTypes.INTEGER,
+        RestaurantId: DataTypes.INTEGER
+    }, {});
+    Comment.associate = function(models) {
+        Comment.belongsTo(models.Restaurant)
+        Comment.belongsTo(models.User)
+    };
+    return Comment;
 };
--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
-  }, {});
-  User.associate = function (models) {
-    // associations can be defined here
-  };
-  return User;
+    const User = sequelize.define('User', {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        isAdmin: DataTypes.BOOLEAN
+    }, {});
+    User.associate = function(models) {
+        User.hasMany(models.Comment)
+    };
+    return User;
 };
