37e376565011625b68a5f4baff016759076a2647
wendy03 <wendysu03@gmail.com>
2020-02-19 02:13:17 +0800

    Add Comment Restaurant on profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const Restaurant = db.Restaurant
+const Comment = db.Comment
 
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
@@ -47,8 +49,12 @@ const userController = {
         res.redirect('/signin')
     },
     getUser: (req, res) => {
-        return User.findByPk(req.params.id, { nest: true, raw: true }).then(user => {
-            return res.render('users/profile', { profile: user })
+        return User.findByPk(req.params.id, {
+            include: [
+                { model: Comment, include: [Restaurant] }
+            ]
+        }).then(user => {
+            return res.render('users/profile', { profile: JSON.parse(JSON.stringify(user)) })
         })
     },
     editUser: (req, res) => {
