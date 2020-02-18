1c85822032e4aac26cdaf4f9182210a29fd6fad3
wendy03 <wendysu03@gmail.com>
2020-02-19 00:50:40 +0800

    Add User Profile Page

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,47 +2,97 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
 
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
 const userController = {
-  signUpPage: (req, res) => {
-    return res.render('signup')
-  },
+    signUpPage: (req, res) => {
+        return res.render('signup')
+    },
 
-  signUp: (req, res) => {
-    if (req.body.passwordCheck != req.body.password) {
-      req.flash('error_messages', '兩次密碼輸入不同!')
-      return res.redirect('/signup')
-    } else {
-      User.findOne({ where: { email: req.body.email } }).then(user => {
-        if (user) {
-          req.flash('error_messages', '信箱重複!')
-          return res.redirect('/signup')
+    signUp: (req, res) => {
+        if (req.body.passwordCheck != req.body.password) {
+            req.flash('error_messages', '兩次密碼輸入不同!')
+            return res.redirect('/signup')
+        } else {
+            User.findOne({ where: { email: req.body.email } }).then(user => {
+                if (user) {
+                    req.flash('error_messages', '信箱重複!')
+                    return res.redirect('/signup')
+                } else {
+                    User.create({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+                    }).then(user => {
+                        return res.redirect('/signin')
+                    })
+                }
+            })
         }
-        else {
-          User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-          }).then(user => {
-            return res.redirect('/signin')
-          })
+    },
+
+    signInPage: (req, res) => {
+        return res.render('signin')
+    },
+
+    signIn: (req, res) => {
+        req.flash('success_messages', '成功登入！')
+        res.redirect('/restaurants')
+    },
+
+    logout: (req, res) => {
+        req.flash('success_messages', '登出成功！')
+        req.logout()
+        res.redirect('/signin')
+    },
+    getUser: (req, res) => {
+        return User.findByPk(req.params.id, { nest: true, raw: true }).then(user => {
+            return res.render('users/profile', { profile: user })
+        })
+    },
+    editUser: (req, res) => {
+        return User.findByPk(req.params.id, { nest: true, raw: true }).then(user => {
+            return res.render('users/edit', { user })
+        })
+    },
+    putUser: (req, res) => {
+        if (Number(req.params.id) !== Number(req.user.id)) {
+            return res.redirect(`/users/${req.params.id}`)
         }
-      })
+        if (!req.user.name) {
+            req.flash('error_messages', "姓名必填寫")
+            return res.redirect('back')
+        }
+
+        const { file } = req
+        if (file) {
+            imgur.setClientID(IMGUR_CLIENT_ID);
+            imgur.upload(file.path, (err, img) => {
+                return User.findByPk(req.params.id)
+                    .then((user) => {
+                        user.update({
+                                name: req.body.name,
+                                image: file ? img.data.link : user.image
+                            })
+                            .then((user) => {
+                                req.flash('success_messages', `${user.name}'s profile was successfully to update`)
+                                res.redirect(`/users/${req.params.id}`)
+                            })
+                    })
+            })
+        } else
+            return User.findByPk(req.params.id)
+                .then((user) => {
+                    user.update({
+                            name: req.body.name,
+                            image: user.image,
+                        })
+                        .then((user) => {
+                            req.flash('success_messages', `${user.name}'s profile was successfully to update`)
+                            res.redirect(`/users/${req.params.id}`)
+                        })
+                })
     }
-  },
-
-  signInPage: (req, res) => {
-    return res.render('signin')
-  },
-
-  signIn: (req, res) => {
-    req.flash('success_messages', '成功登入！')
-    res.redirect('/restaurants')
-  },
-
-  logout: (req, res) => {
-    req.flash('success_messages', '登出成功！')
-    req.logout()
-    res.redirect('/signin')
-  }
 }
 module.exports = userController
