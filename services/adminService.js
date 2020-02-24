f2e2a3621839d462fa6ec6393849168df4446232
wendy03 <wendysu03@gmail.com>
2020-02-24 23:50:42 +0800

    POST /api/admin/restaurants

--- services/adminService.js
+++ services/adminService.js
@@ -15,6 +15,42 @@ const adminService = {
     return Restaurant.findByPk(req.params.id, { nest: true, raw: true, include: [Category] })
       .then(restaurant => callback({ restaurant }))
   },
+  postRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return callback({ status: 'error', message: "Name didn't exist" })
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.create({
+          name: req.body.name,
+          tel: req.body.tel,
+          address: req.body.address,
+          opening_hours: req.body.opening_hours,
+          description: req.body.description,
+          image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
+        }).then((restaurant) => {
+          callback({ status: 'success', message: 'Restaurant was successfully created' })
+        })
+      })
+    } else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        image: null,
+        CategoryId: req.body.categoryId
+      }).then((restaurant) => {
+        callback({ status: 'success', message: 'Restaurant was successfully created' })
+      })
+    }
+  },
   deleteRestaurant: (req, res, callback) => {
     return Restaurant.findByPk(req.params.id)
       .then((restaurant) => {
