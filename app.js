6e282885e076334257a7b1619ebd6a1a7c5af935
wendy03 <wendysu03@gmail.com>
2020-02-24 21:49:45 +0800

    GET /api/admin/restaurants

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const app = express()
 
 const port = process.env.PORT || 3000
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+  require('dotenv').config()
 }
 
 
 // setting template engine
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main',
-    helpers: require('./config/handlebars-helpers')
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
 }))
 app.set('view engine', 'handlebars')
 
@@ -31,15 +31,15 @@ app.use('/upload', express.static(__dirname + '/upload'))
 
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
-    res.locals.success_messages = req.flash('success_messages')
-    res.locals.error_messages = req.flash('error_messages')
-    res.locals.user = req.user
-    next()
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
 })
 
 // start and listen on the Express server
 app.listen(port, () => {
-    console.log(`Express is listening on http://localhost:${port}`)
+  console.log(`Express is listening on http://localhost:${port}`)
 })
 
-require('./routes')(app, passport)
+require('./routes')(app)
