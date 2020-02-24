4e02e207901d2ffa607689eea510b96926bcd1ed
wendy03 <wendysu03@gmail.com>
2020-02-25 00:21:23 +0800

    PUT /api/admin/categories/:id

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -12,6 +12,11 @@ let categoryController = {
       return res.json(data)
     })
   },
+  putCategory: (req, res) => {
+    categoryService.putCategory(req, res, (data) => {
+      return res.json(data)
+    })
+  },
 }
 
 module.exports = categoryController
--- services/categoryService.js
+++ services/categoryService.js
@@ -26,6 +26,19 @@ let categoryService = {
         })
     }
   },
+  putCategory: (req, res, callback) => {
+    if (!req.body.name) {
+      callback({ status: 'error', message: "Name didn't exist" })
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((category) => {
+              callback({ status: 'success', message: 'Restaurant was successfully updated' })
+            })
+        })
+    }
+  },
 }
 
 module.exports = categoryService
